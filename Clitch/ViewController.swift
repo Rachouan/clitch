2613a309298a4d060a37049208f422de3697a000
Said Bachtarysev <said.bachtarysev@hotmail.com>
2018-03-16 20:34:17 +0100

    update

---
+++ Clitch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clitch
+//
+//  Created by Sayed Bachtaryshev on 15/03/2018.
+//  Copyright © 2018 Clitch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
